0feaa0b550b928c010272bb463c0ab32c0b1689f
Camille Fujisaki <cae224@cornell.edu>
2023-02-05 10:07:18 +0900

    added some zazz to the header

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,7 +5,7 @@ export default function Main(props) {
     return (
         <>
             <div className="game-div">
-                <h1>Tenzies</h1>
+                <h1>🎲 Tenzies 🎲</h1>
                 <p className="game-explanation">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
                 {props.diceComponents}
                 <button onClick={props.rollUnheldDice}>{props.tenzies ? 'Play Again' : 'Roll Again'}</button>
