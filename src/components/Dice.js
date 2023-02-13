1fb50cc4a33cf09f70ee5e0d15dbd6122514b465
Camille Fujisaki <cae224@cornell.edu>
2023-02-13 23:32:08 +0900

    adjusted spacing

--- src/components/Dice.js
+++ src/components/Dice.js
@@ -10,14 +10,15 @@ export default function Dice(props) {
   const diceDots = Array.from({length: props.value}, (_, index) => {
     return <Dots key={index}/>
   });
-        return(
-            <div
-                key={props.id}
-                id={`dice-${props.value}`}
-                className={props.held ? "held dice" : "dice"}
-                onClick={props.hold}
-            >
-              {diceDots}
-           </div>
-        )
+
+  return(
+    <div
+        key={props.id}
+        id={`dice-${props.value}`}
+        className={props.held ? "held dice" : "dice"}
+        onClick={props.hold}
+    >
+      {diceDots}
+    </div>
+  )
 }
