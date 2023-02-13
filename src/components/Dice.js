a130121d00f0f740046fb865f536c482261ccde2
Camille Fujisaki <cae224@cornell.edu>
2023-02-13 21:49:06 +0900

    changed dice from number to realistic die faces via css

--- src/components/Dice.js
+++ src/components/Dice.js
@@ -2,13 +2,22 @@ import React from 'react'
 import '../assets/dice.css'
 
 export default function Dice(props) {
+
+  function Dots() {
+    return <span className="dot"></span>
+  }
+
+  const diceDots = Array.from({length: props.value}, (_, index) => {
+    return <Dots key={index}/>
+  });
         return(
             <div
                 key={props.id}
+                id={`dice-${props.value}`}
                 className={props.held ? "held dice" : "dice"}
                 onClick={props.hold}
             >
-            {props.value}
+              {diceDots}
            </div>
         )
 }
