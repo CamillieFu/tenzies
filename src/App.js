f56a23b0f3c6c39957b6cb5f65cd48125c5e9446
Camille Fujisaki <cae224@cornell.edu>
2023-02-13 15:03:55 +0900

    changed tenzies status to state / useEffect instead of regular variable

--- src/App.js
+++ src/App.js
@@ -8,62 +8,65 @@ import useWindowSize from "@rooks/use-window-size"
 export default function App() {
   const [allDice, setAllDice] = React.useState(allNewDice())
   const {width, height} = useWindowSize()
+  const [tenzies, setTenzies] = React.useState(false)
 
-  const tenzies = allDice.every((dice) => (
-      allDice[0].value === dice.value && dice.held ? true : false
-  ))
+React.useEffect(() => {
+    setTenzies(() => {
+      return allDice.every(num => num.value === allDice[0].value && num.held)
+    })
+  }, [allDice])
 
-  function randomNumber() {
-      return Math.ceil(Math.random() * 6);
-  }
+function randomNumber() {
+  return Math.ceil(Math.random() * 6);
+}
 
   function allNewDice() {
-          const newDice = []
-          for(let i = 0; i < 10; i++) {
-              const die = {
-                  held: false,
-                  id: i,
-                  value: randomNumber()
-                  }
-              newDice.push(die)
-          }
-          return newDice
+    const newDice = []
+    for(let i = 0; i < 10; i++) {
+      const die = {
+        held: false,
+        id: i,
+        value: randomNumber()
+        }
+      newDice.push(die)
+    }
+    return newDice
   }
 
   function holdDice(id) {
-      setAllDice(prevDice => prevDice.map(die => {
-          return die.id === id ?
-              {...die, held: !die.held} :
-              die
-      }))
+    setAllDice(prevDice => prevDice.map(die => {
+      return die.id === id ?
+        {...die, held: !die.held} :
+        die
+    }))
   }
 
   const diceComponents = allDice.map((die) => (
-      <Dice key={die.id} {...die} hold={() => holdDice(die.id)} />
+    <Dice key={die.id} {...die} hold={() => holdDice(die.id)} />
   ))
 
   function rollUnheldDice() {
-      if (!tenzies) {
-          setAllDice((prevDice) => prevDice.map(die => {
-              return die.held ?
-              die :
-              {...die, value: randomNumber()}
-          }))
-      } else {
-          setAllDice(allNewDice());
-      }
+    if (!tenzies) {
+      setAllDice((prevDice) => prevDice.map(die => {
+        return die.held ?
+        die :
+        {...die, value: randomNumber()}
+    }))
+    } else {
+        setAllDice(allNewDice());
+    }
   }
 
   return (
-      <div className="game-container">
-      {tenzies &&  <Confetti width={width} height={height} />}
-          <Main
-              dice={allDice}
-              diceComponents={diceComponents}
-              rollUnheldDice={rollUnheldDice}
-              allNewDice={allNewDice}
-              tenzies={tenzies}
-          />
-      </div>
+    <div className="game-container">
+    {tenzies &&  <Confetti width={width} height={height} />}
+        <Main
+            dice={allDice}
+            diceComponents={diceComponents}
+            rollUnheldDice={rollUnheldDice}
+            allNewDice={allNewDice}
+            tenzies={tenzies}
+        />
+    </div>
   )
 }
