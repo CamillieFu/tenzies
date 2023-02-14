d843a19d41ac7e4a9720250f78e4291869d3e0e5
Camille Fujisaki <cae224@cornell.edu>
2023-02-14 20:30:33 +0900

    Added counter function

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,14 +2,23 @@ import React from 'react'
 import '../assets/main.css'
 
 export default function Main(props) {
-    return (
-      <>
-        <div className="game-div">
-            <h1>🎲 Tenzies 🎲</h1>
-            <p className="game-explanation">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
-            {props.diceComponents}
-            <button onClick={props.rollUnheldDice}>{props.tenzies ? 'Play Again' : 'Roll Again'}</button>
-        </div>
-      </>
-    )
+  // track count
+  const [count, setCount] = React.useState(0);
+
+  function handleClick() {
+      setCount(prevCount => prevCount + 1);
+      props.rollUnheldDice();
+  }
+
+  return (
+    <>
+      <div className="game-div">
+          <h1>🎲 Tenzies 🎲</h1>
+          <p className="game-explanation">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+          {props.diceComponents}
+          <button onClick={handleClick}>{props.tenzies ? 'Play Again' : 'Roll Again'}</button>
+          {props.tenzies && <p className="score-count-msg">You completed this round within {count} rolls! Great job!</p>}
+      </div>
+    </>
+  )
 }
